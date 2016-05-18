93dfb75f2c25eb8399eed9754b4e2e978a5e9453
liss <lissete.moscoso@stackops.com>
2016-05-18 14:07:49 +0200

    first commit

---
+++ mydashboard/mypanel/tabs.py
@@ -0,0 +1,52 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import exceptions
+from horizon import tabs
+
+from openstack_dashboard import api
+from openstack_dashboard.dashboards.mydashboard.mypanel import tables
+
+
+class InstanceTab(tabs.TableTab):
+    name = _("Instances Tab")
+    slug = "instances_tab"
+    table_classes = (tables.InstancesTable,)
+    template_name = ("horizon/common/_detail_table.html")
+    preload = False
+
+    def has_more_data(self, table):
+        return self._has_more
+
+    def get_instances_data(self):
+        try:
+            marker = self.request.GET.get(
+                        tables.InstancesTable._meta.pagination_param, None)
+
+            instances, self._has_more = api.nova.server_list(
+                self.request,
+                search_opts={'marker': marker, 'paginate': True})
+
+            return instances
+        except Exception:
+            self._has_more = False
+            error_message = _('Unable to get instances')
+            exceptions.handle(self.request, error_message)
+
+            return []
+
+class MypanelTabs(tabs.TabGroup):
+    slug = "mypanel_tabs"
+    tabs = (InstanceTab,)
+    sticky = True
