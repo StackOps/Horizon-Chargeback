276e4eb95e253520286e3a778e05d744b36ddbbc
Lissete Moscoso León <liss.sb@gmail.com>
2016-07-19 14:48:35 +0200

    update statusMessage directive

--- chargeback_dashboard/static/dashboard/widgets/statusMessage.directive.spec.js
+++ chargeback_dashboard/static/dashboard/widgets/statusMessage.directive.spec.js
@@ -1,83 +1,76 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 (function() {
   'use strict';
-  describe('status Message when the warm is active', function () {
+
+  describe("Status Message Directive", function(){
     var $compile,
-      $scope,
-      controller,
-      element,
-      $q,
-      readFileService;
+    $scope,
+    controller,
+    element,
+    $q,
+    readFileService;
     beforeEach(module('horizon.dashboard.chargeback'));
     beforeEach(module('templates'));
-
     beforeEach(inject(function ($injector) {
       $scope = $injector.get('$rootScope').$new();
       $q = $injector.get('$q');
       $compile = $injector.get('$compile');
-      $scope.warn = true;
-      $scope.message = "This is the message";
-      element = angular.element('<status-message message="message" warn="warn"></status-message>');
-      $compile(element)($scope);
-      $scope.$digest();
-      controller = element.controller('statusMessage');
     }));
 
-    it("should has the id cbk-status-message", function(){
-      var id = element.find('#cbk-status-message');
-      expect(id).toBeDefined();
+    describe('When the warm is active', function () {
+      beforeEach(inject(function ($injector) {
+        $scope.warn = true;
+        $scope.message = "This is the message";
+        element = angular.element('<status-message message="message" warn="warn"></status-message>');
+        $compile(element)($scope);
+        $scope.$digest();
+        controller = element.controller('statusMessage');
+      }));
+
+      it("should has the id cbk-status-message", function(){
+        var id = element.find('#cbk-status-message');
+        expect(id).toBeDefined();
+      });
+
+      it("should have the clas warm active", function(){
+        var id = element.find('#cbk-status-message');
+        expect(id.hasClass('status-message-warn')).toBe(true);
+        expect(id.hasClass('status-message-info')).toBe(false);
+      });
+      it("should show the message", function(){
+        var text = element.find('p');
+        expect(text.text()).toEqual('This is the message');
+      });
     });
 
-    it("should have the clas warm active", function(){
-      var id = element.find('#cbk-status-message');
-      expect(id.hasClass('status-message-warn')).toBe(true);
-      expect(id.hasClass('status-message-info')).toBe(false);
+    describe('When the warm is NOT  active', function () {
+      beforeEach(inject(function ($injector) {
+        $scope.warn = false;
+        $scope.message = "This is the message";
+        element = angular.element('<status-message message="message" warn="warn"></status-message>');
+        $compile(element)($scope);
+        $scope.$digest();
+        controller = element.controller('statusMessage');
+      }));
+
+      it("should have the clas warm active", function(){
+        var id = element.find('#cbk-status-message');
+        expect(id.hasClass('status-message-warn')).toBe(false);
+        expect(id.hasClass('status-message-info')).toBe(true);
+      });
     });
-    it("should show the message", function(){
-      var text = element.find('p');
-      expect(text.text()).toEqual('This is the message');
-    });
+
   });
-
-  describe('status Message when the warm is NOT  active', function () {
-    var $compile,
-      $scope,
-      controller,
-      element,
-      $q,
-      readFileService;
-    beforeEach(module('horizon.dashboard.chargeback'));
-    beforeEach(module('templates'));
-
-    beforeEach(inject(function ($injector) {
-      $scope = $injector.get('$rootScope').$new();
-      $q = $injector.get('$q');
-      $compile = $injector.get('$compile');
-      $scope.warn = false;
-      $scope.message = "This is the message";
-      element = angular.element('<status-message message="message" warn="warn"></status-message>');
-      $compile(element)($scope);
-      $scope.$digest();
-      controller = element.controller('statusMessage');
-    }));
-
-    it("should have the clas warm active", function(){
-      var id = element.find('#cbk-status-message');
-      expect(id.hasClass('status-message-warn')).toBe(false);
-      expect(id.hasClass('status-message-info')).toBe(true);
-    });
-  });
-
 })();
